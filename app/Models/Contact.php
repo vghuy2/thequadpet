51113dcd7a060e11fd21efa6bcdda386b20090f5
hoanghieu12fd-dotcom <hoanghieu12fd@gmail.com>
2026-04-03 12:23:23 +0700

    Lien he model

---
+++ app/Models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'message',
+        'status',
+        'replied_at',
+    ];
+
+    protected $casts = [
+        'replied_at' => 'datetime',
+    ];
+}
